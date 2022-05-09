f1440f374c2ef897a2d91eed40adff378d1645d0
Vincent Courtois <vincent.courtois@coddity.com>
2022-05-09 22:41:46 +0200

    Change the base URI of the controller

--- controllers/campaigns.rb
+++ controllers/campaigns.rb
@@ -1,6 +1,6 @@
 module Controllers
   class Campaigns < Core::Controllers::Base
-    api_route 'get', '/campaigns' do
+    api_route 'get', '/' do
       api_list Core.svc.campaigns.list(account, **params)
     end
   end
